1cc0b2554e8c63b4f520045832e47d53a66e2cc2
tillotoychiyev <tillotoychiyev5@gmail.com>
2026-06-12 08:51:53 +0500

    9-dars

--- api/urls.py
+++ api/urls.py
@@ -1,25 +1,22 @@
-from django.urls import path
-from .views import (DirectorAPIView, DirectorRetrieveAPIView,
-                    CountryAPIView, CountryRetrieveAPIView,
-                    GenreAPIView, GenreRetrieveAPIView,
-                    MovieAPIView, MovieRetrieveAPIView,
-                    CommentAPIView, CommentRetrieveAPIView)
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+from .views import (DirectorAPIViewSet,
+                    CountryAPIViewSet,
+                    GenreAPIViewSet,
+                    MovieAPIViewSet,
+                    CommentAPIViewSet)
+
+router = SimpleRouter()
+router.register('countries', CountryAPIViewSet)
+router.register('movies', MovieAPIViewSet)
+router.register('directors', DirectorAPIViewSet)
+router.register('genres', GenreAPIViewSet)
 
 urlpatterns = [
-    path('directors/', DirectorAPIView.as_view()),
-    path('directors/<int:pk>/', DirectorRetrieveAPIView.as_view(), name='director_detail'),
+    path('movies/<int:movie_id>/comments/',
+    CommentAPIViewSet.as_view({'get':'list', 'post':'create'}), name='comment_list'),
 
-    path('countries/', CountryAPIView.as_view()),
-    path('countries/<int:pk>/', CountryRetrieveAPIView.as_view(), name='country_detail'),
-
-    path('movies/', MovieAPIView.as_view()),
-    path('movies/genre/<int:genre_id>/', MovieAPIView.as_view()),
-    path('movies/director/<int:director_id>/', MovieAPIView.as_view()),
-    path('movies/<int:pk>/', MovieRetrieveAPIView.as_view(), name='movie_detail'),
-
-    path('genres/', GenreAPIView.as_view()),
-    path('genres/<int:pk>/', GenreRetrieveAPIView.as_view(), name='genre_detail'),
-
-    path('movies/<int:movie_id>/comments/', CommentAPIView.as_view()),
-    path('movies/<int:movie_id>/comments/<int:comment_id>/', CommentRetrieveAPIView.as_view()),
+    path('movies/<int:movie_id>/comments/<int:comment_id>/',
+    CommentAPIViewSet.as_view({'get':'retrieve', 'put':'update', 'patch':'partial_update', 'delete':'destroy'}), name='comment_detail'),
+    path('', include(router.urls))
 ]
--- api/views.py
+++ api/views.py
@@ -1,74 +1,124 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework import permissions
 from rest_framework.generics import get_object_or_404
+from rest_framework.viewsets import ModelViewSet
 
 from .serializers import DirectorSerializer, MovieSerializer, GenreSerializer, CountrySerializer, UserSerializer, CommentSerializer
 from .models import Movie, Genre, Director, Country, Comment
 from .permissions import MyIsAuthenticatedOrReadOnly, CommentAuthorPermission
 
-class CountryAPIView(ListCreateAPIView):
+class CountryAPIViewSet(ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
 
-class CountryRetrieveAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Country.objects.all()
-    serializer_class = CountrySerializer
-
-class DirectorAPIView(ListCreateAPIView):
-    queryset = Director.objects.all()
-    serializer_class = DirectorSerializer
-
-class DirectorRetrieveAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Director.objects.all()
-    serializer_class = DirectorSerializer
-
-class MovieAPIView(ListCreateAPIView):
-    serializer_class = MovieSerializer
-    permission_classes = [MyIsAuthenticatedOrReadOnly]
-    def get_queryset(self):
-        genre_id = self.kwargs.get("genre_id")
-        director_id = self.kwargs.get("director_id")
-        if genre_id:
-            return Movie.objects.filter(genre_id=genre_id)
-        if director_id:
-            return Movie.objects.filter(director_id=director_id)
-        return Movie.objects.all()
-
-    def get_serializer_class(self):
-        if self.request.user.is_staff:
-            return MovieSerializer
-        return UserSerializer
-
-class MovieRetrieveAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = [MyIsAuthenticatedOrReadOnly]
+class MovieAPIViewSet(ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
-class GenreAPIView(ListCreateAPIView):
+class DirectorAPIViewSet(ModelViewSet):
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializer
+
+class GenreAPIViewSet(ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
-class GenreRetrieveAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Genre.objects.all()
-    serializer_class = GenreSerializer
-
-class CommentAPIView(ListCreateAPIView):
+class CommentAPIViewSet(MovieAPIViewSet):
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [MyIsAuthenticatedOrReadOnly]
-
+    permission_classes = [
+        MyIsAuthenticatedOrReadOnly,CommentAuthorPermission
+    ]
+    lookup_url_kwarg = 'comment_id'
     def get_queryset(self):
         return Comment.objects.filter(movie_id=self.kwargs.get('movie_id'))
 
     def perform_create(self, serializer):
         movie = get_object_or_404(Movie, pk=self.kwargs.get('movie_id'))
-        serializer.user = self.request.user
         serializer.validated_data['user'] = self.request.user
         serializer.validated_data['movie'] = movie
         serializer.save()
-        return self
+        return serializer
 
-class CommentRetrieveAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [MyIsAuthenticatedOrReadOnly, CommentAuthorPermission]
-    lookup_url_kwarg = 'comment_id'
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# class CountryAPIView(ListCreateAPIView):
+#     queryset = Country.objects.all()
+#     serializer_class = CountrySerializer
+#
+# class CountryRetrieveAPIView(RetrieveUpdateDestroyAPIView):
+#     queryset = Country.objects.all()
+#     serializer_class = CountrySerializer
+
+
+# class DirectorAPIView(ListCreateAPIView):
+#     queryset = Director.objects.all()
+#     serializer_class = DirectorSerializer
+#
+# class DirectorRetrieveAPIView(RetrieveUpdateDestroyAPIView):
+#     queryset = Director.objects.all()
+#     serializer_class = DirectorSerializer
+
+
+#
+# class MovieAPIView(ListCreateAPIView):
+#     serializer_class = MovieSerializer
+#     permission_classes = [MyIsAuthenticatedOrReadOnly]
+#     def get_queryset(self):
+#         genre_id = self.kwargs.get("genre_id")
+#         director_id = self.kwargs.get("director_id")
+#         if genre_id:
+#             return Movie.objects.filter(genre_id=genre_id)
+#         if director_id:
+#             return Movie.objects.filter(director_id=director_id)
+#         return Movie.objects.all()
+#
+#     def get_serializer_class(self):
+#         if self.request.user.is_staff:
+#             return MovieSerializer
+#         return UserSerializer
+#
+# class MovieRetrieveAPIView(RetrieveUpdateDestroyAPIView):
+#     permission_classes = [MyIsAuthenticatedOrReadOnly]
+#     queryset = Movie.objects.all()
+#     serializer_class = MovieSerializer
+#
+# class GenreAPIView(ListCreateAPIView):
+#     queryset = Genre.objects.all()
+#     serializer_class = GenreSerializer
+#
+# class GenreRetrieveAPIView(RetrieveUpdateDestroyAPIView):
+#     queryset = Genre.objects.all()
+#     serializer_class = GenreSerializer
+#
+# class CommentAPIView(ListCreateAPIView):
+#     serializer_class = CommentSerializer
+#     permission_classes = [MyIsAuthenticatedOrReadOnly]
+#
+#     def get_queryset(self):
+#         return Comment.objects.filter(movie_id=self.kwargs.get('movie_id'))
+#
+#     def perform_create(self, serializer):
+#         movie = get_object_or_404(Movie, pk=self.kwargs.get('movie_id'))
+#         serializer.user = self.request.user
+#         serializer.validated_data['user'] = self.request.user
+#         serializer.validated_data['movie'] = movie
+#         serializer.save()
+#         return self
+#
+# class CommentRetrieveAPIView(RetrieveUpdateDestroyAPIView):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+#     permission_classes = [MyIsAuthenticatedOrReadOnly, CommentAuthorPermission]
+#     lookup_url_kwarg = 'comment_id'
